11fca1c2978e5979edfbc6388d3feb060596af81
kashyapdhamecha2207 <kashyap.dhamecha.cg@gmail.com>
2026-01-04 23:50:11 +0530

    Bitwise Operator Learned

--- L7.Bitwise-Operator/code.cpp
+++ L7.Bitwise-Operator/code.cpp
@@ -21,11 +21,45 @@
 
 // Left Shift
 
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     // cout << (4 << 1)<< endl;          // 8
+//     // cout << (10 << 2)<< endl;          // 40
+//     cout << (8 >> 2) << endl;             // 2
+//     return 0;
+// }
+
+
+
+// Operator Precendence
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+    
+//     cout << (5 - 2*6) << endl;
+//     return 0;
+// }
+
+
+
+
+// Data Type Modifiers
+
+// 4 bytes = 32 bites
+
+
 #include <iostream>
 using namespace std;
 
 int main(){
-    cout << (4 << 1)<< endl;
-    cout << (10 << 2)<< endl;
+    
+    cout << sizeof(int) << endl;
+    cout << sizeof(long int) << endl;
+    cout << sizeof(short int) << endl;
+    cout << sizeof(long long) << endl;
     return 0;
 }
