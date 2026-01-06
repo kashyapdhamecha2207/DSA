d5947f5f3c99336dc9f70c7c8407fa83743991b5
kashyapdhamecha2207 <kashyap.dhamecha.cg@gmail.com>
2026-01-06 23:00:03 +0530

    changes made

--- L6.Binary-Number/code.cpp
+++ L6.Binary-Number/code.cpp
@@ -1,54 +1,54 @@
-// Decimal To Binary
+// // Decimal To Binary
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int decToBinary(int decNum){
-    int ans = 0, pow = 1;
+// int decToBinary(int decNum){
+//     int ans = 0, pow = 1;
 
-    while(decNum > 0){
-        int rem = decNum%2;
-        decNum = decNum/2;
-        ans += (rem * pow);
-        pow *= 10;
-    }
+//     while(decNum > 0){
+//         int rem = decNum%2;
+//         decNum = decNum/2;
+//         ans += (rem * pow);
+//         pow *= 10;
+//     }
 
-    return ans;        // 110010
-}
+//     return ans;        // 110010
+// }
 
-int main(){
-    int decNum = 50;
-    cout << decToBinary(decNum) << endl;
-    cout << "1 to 10 binary\n";
-    for(int i =1; i<=10 ; i++){
-        cout << decToBinary(i) <<endl;
-    }
+// int main(){
+//     int decNum = 50;
+//     cout << decToBinary(decNum) << endl;
+//     cout << "1 to 10 binary\n";
+//     for(int i =1; i<=10 ; i++){
+//         cout << decToBinary(i) <<endl;
+//     }
 
-    return 0;    
-};
+//     return 0;    
+// };
 
 
-// Binary To Decimal
+// // Binary To Decimal
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int binaryToDec(int binaryNum){
-    int ans = 0, pow = 1;
+// int binaryToDec(int binaryNum){
+//     int ans = 0, pow = 1;
 
-    while(binaryNum > 0){
-        int rem = binaryNum%10;
-        ans += (rem * pow);
-        binaryNum = binaryNum/10;
-        pow *= 2;
-    }
+//     while(binaryNum > 0){
+//         int rem = binaryNum%10;
+//         ans += (rem * pow);
+//         binaryNum = binaryNum/10;
+//         pow *= 2;
+//     }
 
-    return ans;       // 
-}
+//     return ans;       // 
+// }
 
-int main(){
-    int binaryNum = 110010;
-    cout << binaryToDec(binaryNum) << endl;
+// int main(){
+//     int binaryNum = 110010;
+//     cout << binaryToDec(binaryNum) << endl;
 
-    return 0;    
-};
+//     return 0;    
+// };
